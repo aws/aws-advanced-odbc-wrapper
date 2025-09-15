56cd7f2339773210fee295069c9d1c64cc78ea3a
Colin <70862826+ColinKYuen@users.noreply.github.com>
2025-09-15 15:13:36 -0700

    feat: validate values for ints (#20)

---
+++ driver/util/attribute_validator.cpp
@@ -0,0 +1,64 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "attribute_validator.h"
+
+#include <unordered_set>
+#include <map>
+
+#include "connection_string_keys.h"
+#include "rds_strings.h"
+
+bool AttributeValidator::ShouldKeyBeInt(const RDS_STR& key) {
+    static const std::unordered_set<RDS_STR> int_keys = {
+        KEY_PORT,
+        KEY_TOKEN_EXPIRATION,
+        KEY_IAM_PORT,
+        KEY_IDP_PORT,
+        KEY_HTTP_SOCKET_TIMEOUT,
+        KEY_HTTP_CONNECT_TIMEOUT,
+        KEY_IGNORE_TOPOLOGY_REQUEST,
+        KEY_HIGH_REFRESH_RATE,
+        KEY_REFRESH_RATE,
+        KEY_FAILOVER_TIMEOUT,
+        KEY_LIMITLESS_MONITOR_INTERVAL_MS
+    };
+    return int_keys.contains(key);
+}
+
+bool AttributeValidator::IsValueInt(const RDS_STR& value) {
+    if (value.empty()) return false;
+    try {
+        std::size_t pos{};
+        std::stoi(value, &pos);
+        return pos == value.length();
+    } catch (std::invalid_argument) {
+        return false;
+    } catch (std::out_of_range) {
+        return false;
+    }
+}
+
+std::unordered_set<RDS_STR> AttributeValidator::ValidateMap(const std::map<RDS_STR, RDS_STR>& conn_attr) {
+    std::unordered_set<RDS_STR> invalid_keys;
+    for (const auto& e : conn_attr) {
+        RDS_STR key = e.first;
+        RDS_STR value = e.second;
+
+        if (ShouldKeyBeInt(key) && !IsValueInt(value)) {
+            invalid_keys.insert(key);
+        }
+    }
+    return invalid_keys;
+}
---
+++ test/unit_test/attribute_validator_test.cpp
@@ -0,0 +1,88 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <map>
+
+#include "../../driver/util/attribute_validator.h"
+#include "../../driver/util/connection_string_keys.h"
+
+class AttributeValidatorTest : public testing::Test {
+protected:
+    // Runs once per suite
+    static void SetUpTestSuite() {}
+    static void TearDownTestSuite() {}
+
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(AttributeValidatorTest, ShouldKeyBeInt) {
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_TOKEN_EXPIRATION));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_IAM_PORT));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_IDP_PORT));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_HTTP_SOCKET_TIMEOUT));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_HTTP_CONNECT_TIMEOUT));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_IGNORE_TOPOLOGY_REQUEST));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_HIGH_REFRESH_RATE));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_REFRESH_RATE));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_FAILOVER_TIMEOUT));
+    EXPECT_TRUE(AttributeValidator::ShouldKeyBeInt(KEY_LIMITLESS_MONITOR_INTERVAL_MS));
+
+    EXPECT_FALSE(AttributeValidator::ShouldKeyBeInt(KEY_DATABASE));
+    EXPECT_FALSE(AttributeValidator::ShouldKeyBeInt(KEY_SERVER));
+    EXPECT_FALSE(AttributeValidator::ShouldKeyBeInt(KEY_DRIVER));
+    EXPECT_FALSE(AttributeValidator::ShouldKeyBeInt(KEY_AUTH_TYPE));
+    EXPECT_FALSE(AttributeValidator::ShouldKeyBeInt(KEY_REGION));
+}
+
+TEST_F(AttributeValidatorTest, IsValueInt) {
+    EXPECT_TRUE(AttributeValidator::IsValueInt("1234"));
+    EXPECT_TRUE(AttributeValidator::IsValueInt("-1234"));
+
+    EXPECT_FALSE(AttributeValidator::IsValueInt("99999999999999999999"));
+
+    EXPECT_FALSE(AttributeValidator::IsValueInt("abc"));
+    EXPECT_FALSE(AttributeValidator::IsValueInt("abc123efg"));
+    EXPECT_FALSE(AttributeValidator::IsValueInt("1e2"));
+    EXPECT_FALSE(AttributeValidator::IsValueInt("123efg"));
+}
+
+TEST_F(AttributeValidatorTest, ValidateMap) {
+    std::unordered_set<std::string> invalid_params_set;
+
+    std::map<std::string, std::string> valid_map = {
+        {KEY_PORT, "1234"},
+        {KEY_REFRESH_RATE, "5678"},
+        {KEY_SERVER, "my-db-host"}
+    };
+    invalid_params_set = AttributeValidator::ValidateMap(valid_map);
+    EXPECT_TRUE(invalid_params_set.empty());
+
+    std::map<std::string, std::string> invalid_map = {
+        {KEY_PORT, "5 hundred"},
+        {KEY_REFRESH_RATE, "million"},
+        {KEY_FAILOVER_TIMEOUT, "1234"},
+        {KEY_SERVER, "my-db-host"}
+    };
+    std::unordered_set<std::string> expected_invalid_params = {
+        KEY_PORT, KEY_REFRESH_RATE
+    };
+    invalid_params_set = AttributeValidator::ValidateMap(invalid_map);
+    for (std::string expected : expected_invalid_params) {
+        EXPECT_TRUE(invalid_params_set.contains(expected));
+    }
+}
